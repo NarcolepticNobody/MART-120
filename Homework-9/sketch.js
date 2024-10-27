650029bdd85fe56716ff0fea9d25ebe842e8ec3c
NarcolepticNobody <alexis.huotte@umontana.edu>
2024-10-27 15:33:46 -0600

    p5

---
+++ Homework-9/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+    createCanvas(500, 600);
+  }
+  
+  function draw() {
+    background(11, 200, 900);
+    fill(100,300, 0);  
+    circle(250,100,175)
+    fill(199, 14, 50);
+    rect(225,188,50,200);
+    
+    rect(200,380,45,190);
+    
+    rect(250,380,45,190);
+    fill(50,3,0);
+     circle(240,100,30);
+     fill(50,3,0);
+     circle(280,100,30);
+     rect(200,120,60,20);
+    
+   
+    line(200, 60, 90, 300);
+  ellipse(120,250,200,60);
+    
+    strokeWeight(5);
+    point(200, 100);
+    line(200, 20, 100, 200);
+    ellipse(380,200,200,60);
+    point(330, 100);
+    
+    triangle(100, 400, 250, 300,400, 400);
+     square(0,200,50);
+     square(450,150,50);
+    textSize(32);
+    text('Alexis Huotte', 300, 550);
+    textSize(32);
+      text('Shape Lady!', 10, 30);
+   
+    
+  }
